61456c1b949a6e757599d0d3d08c61f1b7889bec
Hao Hsu <luke@HaodeMacBook-Air-2.local>
2026-05-16 14:36:17 +0800

    feat: assemble home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import Hero from "@/components/home/Hero";
+import Story from "@/components/home/Story";
+import Products from "@/components/home/Products";
+import Science from "@/components/home/Science";
+import OEMSection from "@/components/home/OEMSection";
+import Closing from "@/components/home/Closing";
+
 export default function Home() {
-  return <main>hello</main>;
+  return (
+    <main>
+      <Hero />
+      <Story />
+      <Products />
+      <Science />
+      <OEMSection />
+      <Closing />
+    </main>
+  );
 }
